b5f9099d71225e9bbcc7d45f5aa3342c96efb560
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-20 00:31:41 +0200

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar';
+import { BrowserRouter } from 'react-router-dom';
+
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <NavBar />
     </div>
+    </BrowserRouter>
   );
 }
 
