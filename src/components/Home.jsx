135a86a569ce1094c35bff6622fe243687c55f52
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-21 00:44:04 +0200

    interacting with the global store

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import '../styles/home.css'
+import React, { Component } from 'react';
+import '../styles/home.css';
+import { connect } from 'react-redux';
 
-const Home = () => {
-    return (
-        <h1 className='home'>Welcome to our Blog site.</h1>
-    )
+class Home extends Component {
+
+    render() {
+        const { posts } = this.props;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return(
+                    <div className="postCard" key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.content}</p>
+                    </div>
+                )
+            })
+        ) : (<h1>No posts!</h1>);
+        return (
+            <div className="home">
+                <h1>Home</h1>
+                {postList}
+            </div>
+
+        )
+    }
 }
 
-export default Home;
+const mapStateToProps = state => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home);
