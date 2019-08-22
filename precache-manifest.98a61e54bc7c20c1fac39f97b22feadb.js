8bbb30420503aad71264c06def4e263fb3135fb4
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-22 02:33:51 +0200

    Updates

---
+++ precache-manifest.98a61e54bc7c20c1fac39f97b22feadb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab88fbc02af883d1f49ad21a07f0d1c",
+    "url": "/Blogging_App/index.html"
+  },
+  {
+    "revision": "ec626ce07e585e90f3ce",
+    "url": "/Blogging_App/static/css/main.8363d5e2.chunk.css"
+  },
+  {
+    "revision": "968278ef9fc7daa05bf2",
+    "url": "/Blogging_App/static/js/2.479c171e.chunk.js"
+  },
+  {
+    "revision": "ec626ce07e585e90f3ce",
+    "url": "/Blogging_App/static/js/main.fb068086.chunk.js"
+  },
+  {
+    "revision": "e4244873b446d2c812e7",
+    "url": "/Blogging_App/static/js/runtime~main.d4b43d13.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Blogging_App/precache-manifest.98a61e54bc7c20c1fac39f97b22feadb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Blogging_App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
